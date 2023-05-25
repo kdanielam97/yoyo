f3347308100f6125153ceeb9d5d2b0311599e837
Dan1elFelipe <danielfelipeluborealpe@gmail.com>
2023-05-24 21:20:55 -0500

    ejemplo1

---
+++ ejercicio1.py
@@ -0,0 +1,15 @@
+"""Cree las variables y asigne los siguientes valores y Muestre la información en consola
+de los datos ingresados"""
+
+nombre="daniela"
+edad=int(input("ingresar edad: "))
+identificacion="1061804834"
+mayor_de_edad=edad>=18
+
+if edad >=18:
+    print("bienvenida")
+
+else:
+    print("no puede ingresar")
+
+
